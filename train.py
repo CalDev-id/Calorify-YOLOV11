f3283aebd21db898e7a442a0b038c7fc94358d45
Heical Chandra <haical31@gmail.com>
2025-03-28 11:17:05 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11s.pt")
-model.train(data="config.yaml", epochs=5, batch=4, imgsz=640)
+model.train(data="config.yaml", epochs=1, batch=4, imgsz=640)
 
 # Save the best model
 trained_model_path = "runs/detect/train/weights/best.pt"
@@ -11,5 +11,5 @@ trained_model_path = "runs/detect/train/weights/best.pt"
 model = YOLO(trained_model_path)
 
 # Export to ONNX format
-model.export(format="onnx", dynamic=True)  # This creates best.onnx
+model.export(format="tflite")  # This creates best.onnx
 
