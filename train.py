84823e2fffdb8f6d9db3e9ddc88375dfe8fd2fb1
Heical Chandra <haical31@gmail.com>
2025-03-28 10:42:10 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11s.pt")
-model.train(data="config.yaml", epochs=50, batch=4, imgsz=640)
+model.train(data="config.yaml", epochs=5, batch=4, imgsz=640)
 
 # Save the best model
 trained_model_path = "runs/detect/train/weights/best.pt"
