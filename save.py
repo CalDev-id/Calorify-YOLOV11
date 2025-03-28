60ab3cb369378676339ca433c2e092499c1131ee
Heical Chandra <haical31@gmail.com>
2025-03-28 19:00:34 +0700

    update

---
+++ save.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+# Save the best model
+trained_model_path = "runs/detect/train/weights/best.pt"
+
+
+# Load trained YOLO model
+model = YOLO(trained_model_path)
+
+# Export to ONNX format
+model.export(format="tflite")  # This creates best.onnx
--- train.py
+++ train.py
@@ -3,6 +3,8 @@ from ultralytics import YOLO
 model = YOLO("yolo11s.pt")
 model.train(data="config.yaml", epochs=1, batch=4, imgsz=640)
 
+
+
 # Save the best model
 trained_model_path = "runs/detect/train/weights/best.pt"
 
@@ -12,4 +14,3 @@ model = YOLO(trained_model_path)
 
 # Export to ONNX format
 model.export(format="tflite")  # This creates best.onnx
-
